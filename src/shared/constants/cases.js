e9c12bb2c5f58ade46bfe840f49081e236b43dc6
fnajer <lexactr@gmail.com>
2020-02-25 16:47:10 +0300

    add constants data

---
+++ src/shared/constants/cases.js
@@ -0,0 +1,21 @@
+import cryptoSvg from 'shared/icons/cases/crypto.svg';
+import giftSvg from 'shared/icons/cases/gift.svg';
+import moneySvg from 'shared/icons/cases/money.svg';
+
+export default [
+  {
+    heading: 'Crypto-payments, using an ordinary wallet',
+    img: cryptoSvg,
+    description: 'BIP can be used for immediate payment for goods and services at will',
+  },
+  {
+    heading: 'Gift-cards, loyalty programs',
+    img: giftSvg,
+    description: 'BitTeam provides businesses with a means of building trust-based relations with a customer',
+  },
+  {
+    heading: 'Communities’ money',
+    img: moneySvg,
+    description: 'Coins can be used for transferring values within a particular community',
+  },
+]
